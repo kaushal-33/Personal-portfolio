893058db6ad6fd0bd568c91d01b0fdc90f451ccf
kaushal-33 <varmakaushal33@gmail.com>
2024-07-30 18:46:55 +0530

    here is my personal portfolio website.

---
+++ assets/js/headline.js
@@ -0,0 +1,39 @@
+//you can change the text, but you need to keep same format [yourtext, color];
+let yourtext = [
+    ["WEB DEVELOPER", "#27a776"]
+  ];
+  
+  let x = 0;
+  let y = 0;
+  //how fast typing is
+  let wait = 300;
+  //how long you want to text stay before overwriting
+  let additionalwait = 5;
+  let txt = yourtext[0][0].split("");
+  let out = "";
+  let retyping = setInterval(function () {
+    document.getElementById("changingText").innerHTML = out;
+    if (x > txt.length - 1) {
+    } else {
+      out += txt[x];
+    }
+    x++;
+    if (x == txt.length + 2 + additionalwait) {
+      if (y == yourtext.length - 1) {
+        y = 0;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      } else {
+        y += 1;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      }
+    }
+  }, wait);
+  
